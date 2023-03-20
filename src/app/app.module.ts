f508d40d71799a12009602318aacc761a08f1e48
lucas <lucas.czeck@inteliger.com.br>
2023-03-20 20:49:57 -0300

    Criado component summary para parte de resumo que ficará na esquerda da tela inicial, menu retirado de dentro do header. TODO: Ajustar abertura do menu para sobrepor tudo ao invês de empurrar para baixo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/main/header/header.component';
+import { SummaryComponent } from './core/main/summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
