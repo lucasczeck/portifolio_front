fe14da22acdbc755553f1cd0b626cb119e89974c
lucas <lucas.czeck@inteliger.com.br>
2023-05-22 19:03:54 -0300

    component de informações pessoais

--- src/app/core/main/aboutme/personal-infos/personal-infos.component.ts
+++ src/app/core/main/aboutme/personal-infos/personal-infos.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-personal-infos',
   templateUrl: './personal-infos.component.html',
   styleUrls: ['./personal-infos.component.css']
 })
-export class PersonalInfosComponent {
 
+export class PersonalInfosComponent implements OnInit{
+  @Input() data!: any
+  
+  constructor() {}
+
+  ngOnInit(): void {}
 }
